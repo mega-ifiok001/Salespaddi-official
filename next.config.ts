64f8b1b507a6b727d62d0ba75e232abb3f9967f4
Aniebiet (mega) ifiok <ifiokaniebiet@gmail.com>
2026-07-02 16:12:46 +0100

    Add allowed origin for salespaddi official app
    Add allowed origin for salespaddi official app

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,7 @@ const nextConfig: NextConfig = {
       allowedOrigins: [
         'ubiquitous-computing-machine-jj5rvvgrw555fj5q7-3000.app.github.dev',
         'localhost:3000',
+        "https://salespaddi-official.vercel.app/"
       ],
     },
   },
